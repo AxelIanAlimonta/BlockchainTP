baf335141dfabe50a5c3e1d776b8c649cf357ff0
martin <guerreiromartin@gmail.com>
2024-11-03 23:47:13 -0300

    cambios de estilos

--- SistemaDeVotacion.web/SistemaDeVotacion.Domain/obj/Debug/net8.0/SistemaDeVotacion.Domain.AssemblyInfo.cs
+++ SistemaDeVotacion.web/SistemaDeVotacion.Domain/obj/Debug/net8.0/SistemaDeVotacion.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeVotacion.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc92f53c057a92bd10df122249deb09cf6145550")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f547e88b87d52b4cb87ca0b18308ac9ca6d66a6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDeVotacion.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeVotacion.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
